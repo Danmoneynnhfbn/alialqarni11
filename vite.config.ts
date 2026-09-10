0632deb5fb7eabdae0e06b26fefa6b5c5e992233
Daniel Concept <105176908+Danmoneynnhfbn@users.noreply.github.com>
2026-09-10 17:45:05 +0100

    loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({ base: './', server: { port: 5500 }, build: { rollupOptions: { input: 'index.source.html' } }, plugins: [react()] })
